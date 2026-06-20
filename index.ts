0e83675298c684f8353f6f60b69445840d7ac360
William Olson <willko747@gmail.com>
2026-06-20 12:08:31 -0500

    surface fail type enum

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export type { AppState } from "./src/core/state";
+export type { AppState, FailType } from "./src/core/state";
 export { Task } from "./src/core/task";
 export type { TaskClass } from "./src/core/task";
 export { CommandService } from "./src/core/command-service";
